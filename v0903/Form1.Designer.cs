a27a9c70b9d0f3fde1a1cbb28e8cae49c42fa5f3
A601-22 <tnk.dat.class@gmail.com>
2020-09-03 12:21:57 +0900

    演習15-1

--- v0903/Form1.Designer.cs
+++ v0903/Form1.Designer.cs
@@ -32,26 +32,32 @@
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20F);
-            this.label1.Location = new System.Drawing.Point(98, 113);
+            this.label1.Location = new System.Drawing.Point(12, 316);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 27);
+            this.label1.Size = new System.Drawing.Size(108, 27);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "(´・ω・`)";
+            this.label1.Visible = false;
             // 
             // timer1
             // 
+            this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("MS UI Gothic", 50F);
-            this.button1.Location = new System.Drawing.Point(193, 171);
+            this.button1.Location = new System.Drawing.Point(199, 169);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(297, 174);
             this.button1.TabIndex = 1;
@@ -59,11 +65,59 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.label2.Location = new System.Drawing.Point(12, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "(´・ω・`)";
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.label3.Location = new System.Drawing.Point(12, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 27);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "(´・ω・`)";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 50F);
+            this.label4.Location = new System.Drawing.Point(159, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(365, 67);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "★CLEAR★";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.label5.Location = new System.Drawing.Point(12, 482);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 27);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Score=0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 518);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -78,6 +132,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- v0903/Form1.cs
+++ v0903/Form1.cs
@@ -13,46 +13,159 @@ namespace v0903
     public partial class Form1 : Form
     {
         private static Random rand = new Random();
-        int vx = rand.Next(-20, 21);
-        int vy = rand.Next(-20, 21);
+        int[] vx = new int[100];
+        int[] vy = new int[100];
+        Label[] labels = new Label[100];
+        int score = 0;
         public Form1()
         {
             InitializeComponent();
-            label1.Left = rand.Next(0,ClientSize.Width - label1.Width);
-            label1.Top = rand.Next(0,ClientSize.Height - label1.Height);
+
+            for (int i = 0; i < 3; i++)
+            {
+                vx[i] = rand.Next(-10, 11);
+                vy[i] = rand.Next(-10, 11);
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             Point mp = MousePosition;
             mp = PointToClient(mp);
-            label1.Left += vx;
-            label1.Top += vy;
+            score++;
+            label5.Text = "Score:" + score;
+            for (int i = 0; i < 100; i++)
+            {
+                labels[i].Left += vx[i];
+                labels[i].Top += vy[i];
+                if (labels[i].Left < 0)
+                {
+                    vx[i] = Math.Abs(vx[i]);
+                }
+                if (labels[i].Right > ClientSize.Width)
+                {
+                    vx[i] = -Math.Abs(vx[i]);
+                }
+                if (labels[i].Top < 0)
+                {
+                    vy[i] = Math.Abs(vy[i]);
+                }
+                if (labels[i].Bottom > ClientSize.Height)
+                {
+                    vy[i] = -Math.Abs(vy[i]);
+                }
+                if (labels[i].Left < mp.X && labels[i].Right >= mp.X && labels[i].Top < mp.Y && labels[i].Bottom >= mp.Y)
+                {
+                    labels[i].Visible = false;
+                }
+            }
+            label1.Left += vx[0];
+            label1.Top += vy[0];
             if (label1.Left < 0)
             {
-                vx = Math.Abs(vx);
+                vx[0] = Math.Abs(vx[0]);
             }
             if (label1.Right > ClientSize.Width)
             {
-                vx = -Math.Abs(vx);
+                vx[0] = -Math.Abs(vx[0]);
             }
             if (label1.Top < 0)
             {
-                vy = Math.Abs(vy);
+                vy[0] = Math.Abs(vy[0]);
             }
             if (label1.Bottom > ClientSize.Height)
             {
-                vy = -Math.Abs(vy);
+                vy[0] = -Math.Abs(vy[0]);
             }
-            if(label1.Left<mp.X&&label1.Right>=mp.X&&label1.Top<mp.Y&&label1.Bottom>=mp.Y)
+            if (label1.Left < mp.X && label1.Right >= mp.X && label1.Top < mp.Y && label1.Bottom >= mp.Y)
+            {
+                label1.Visible = false;
+            }
+            label2.Left += vx[1];
+            label2.Top += vy[1];
+            if (label2.Left < 0)
+            {
+                vx[1] = Math.Abs(vx[1]);
+            }
+            if (label2.Right > ClientSize.Width)
+            {
+                vx[1] = -Math.Abs(vx[1]);
+            }
+            if (label2.Top < 0)
+            {
+                vy[1] = Math.Abs(vy[1]);
+            }
+            if (label2.Bottom > ClientSize.Height)
+            {
+                vy[1] = -Math.Abs(vy[1]);
+            }
+            if (label2.Left < mp.X && label2.Right >= mp.X && label2.Top < mp.Y && label2.Bottom >= mp.Y)
+            {
+                label2.Visible = false;
+            }
+            label3.Left += vx[2];
+            label3.Top += vy[2];
+            if (label3.Left < 0)
+            {
+                vx[2] = Math.Abs(vx[2]);
+            }
+            if (label3.Right > ClientSize.Width)
+            {
+                vx[2] = -Math.Abs(vx[2]);
+            }
+            if (label3.Top < 0)
+            {
+                vy[2] = Math.Abs(vy[2]);
+            }
+            if (label3.Bottom > ClientSize.Height)
+            {
+                vy[2] = -Math.Abs(vy[2]);
+            }
+            if (label3.Left < mp.X && label3.Right >= mp.X && label3.Top < mp.Y && label3.Bottom >= mp.Y)
+            {
+                label3.Visible = false;
+            }
+            if(label1.Visible==false&&label2.Visible==false&&label3.Visible==false)
             {
                 timer1.Enabled = false;
+                label4.Visible = true;
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            score = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                vx[i] = rand.Next(-10, 11);
+                vy[i] = rand.Next(-10, 11);
+                vx[i] = rand.Next(-10, 11);
+                vy[i] = rand.Next(-10, 11);
+                labels[i] = new Label();
+                labels[i].AutoSize = true;
+                labels[i].Text = "(´・ω・`)";
+                Controls.Add(labels[i]);
+                labels[i].Left = rand.Next(0, ClientSize.Width - label1.Width);
+                labels[i].Top = rand.Next(0, ClientSize.Height - label1.Height);
+            }
+
+            label1.Left = rand.Next(0, ClientSize.Width - label1.Width);
+            label1.Top = rand.Next(0, ClientSize.Height - label1.Height);
+            label2.Left = rand.Next(0, ClientSize.Width - label2.Width);
+            label2.Top = rand.Next(0, ClientSize.Height - label2.Height);
+            label3.Left = rand.Next(0, ClientSize.Width - label3.Width);
+            label3.Top = rand.Next(0, ClientSize.Height - label3.Height);
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
             timer1.Enabled = true;
+            button1.Visible = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
