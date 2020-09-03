dfe3dcf5b7c60637e268bdd1f039d8142451cf4d
A601-22 <tnk.dat.class@gmail.com>
2020-09-03 09:35:23 +0900

    復習問題ー1

--- v0903/Form1.cs
+++ v0903/Form1.cs
@@ -12,9 +12,20 @@ namespace v0903
 {
     public partial class Form1 : Form
     {
+        private static Random rand = new Random();
+        int vx = rand.Next(-20, 21);
+        int vy = rand.Next(-20, 21);
         public Form1()
         {
             InitializeComponent();
+            label1.Left = rand.Next(0,ClientSize.Width - label1.Width);
+            label1.Top = rand.Next(0,ClientSize.Height - label1.Height);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
         }
     }
 }
